fb960d84b6c473fc9a014355bee9e138dfdf3b54
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2020-11-05 13:33:24 +0900

    js practice

--- JS practice/dom/domevents/script.js
+++ JS practice/dom/domevents/script.js
@@ -2,29 +2,44 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+// 입력값 길이
 function inputLength() {
 	return input.value.length;
 }
 
+// li 붙이기
 function createListElement() {
 	var li = document.createElement("li");
+	//element생성 
+
 	li.appendChild(document.createTextNode(input.value));
+	//돔 구조상 하위 노드에 잇는 text를 넣어주는 작업
+
 	ul.appendChild(li);
+	//생성한 element를 붙이는 작업
+
 	input.value = "";
 }
 
+// 입력한 데이터 길이가 0보다 큰지
 function addListAfterClick() {
+	//alert("thanks");
+	//콜백함수 실험
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
+// 엔터키
 function addListAfterKeypress(event) {
+	//console.log(event); 모든 이벤트 출력 42번째줄 때문에 keypress이벤트만 나옴
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+button.addEventListener("click", addListAfterClick());
+//2번째 파라미터는 함수, ()를 뒤에 붙이면 안된다, 붙이면 로드하는 중 1번만 자동 실행된 후 더 이상 작업하지 않는다
+//이런 형식을 콜백함수라고 한다
 
 input.addEventListener("keypress", addListAfterKeypress);
