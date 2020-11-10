b2fba9af2a6f7886850f527b1f7dfd385cb72ada
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2020-11-10 13:53:21 +0900

    js practice

--- JS practice/advanced/advanced arrays/exercise5.js
+++ JS practice/advanced/advanced arrays/exercise5.js
@@ -37,20 +37,35 @@ console.log(firstresult);
 
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
-
+const second = array.map(a => {
+  a.username += '?';
+  return a;
+})
+console.log(second);
 
 //Filter the array to only include users who are on team: red
-
+const third = array.filter(a => a.team == 'red');
+console.log(third);
 
 //Find out the total score of all users using reduce
+const fourth = array.reduce((start, sum) => {
+  return start + sum.score
+}, 0);
+console.log(fourth);
 
-// (1), what is the value of i?
+// (1), what is the value of i? Index!!!!
 // (2), Make this map function pure:
-// const arrayNum = [1, 2, 4, 5, 8, 9];
-// const newArray = arrayNum.map((num, i) => {
-// 	console.log(num, i);
-// 	alert(num);
-// 	return num * 2;
-// })
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray = arrayNum.map((num, i) => {
+	return num * 2;
+})
+
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+const answer = array.map(user => {
+  user.items = user.items.map(item => {
+    return item + "!"
+  });
+  return user;
+})
+console.log(answer);
