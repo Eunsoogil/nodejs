06198d2283360b97bd482df95a4e538bc8ec8adf
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2020-12-08 08:52:17 +0900

    react practice

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+//import Clarifai from 'Clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
-import Particles from 'react-particles-js';
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '018bcfc87ef245509d7fde1d65094cc8'
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +26,27 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict("@@sampleTrain");
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,7 +56,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         {/*<FaceRecognition />*/}
       </div>
     );
