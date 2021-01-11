451fdbf804f020dc69c76a34155a19bef5970edf
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-01-12 08:58:32 +0900

    node js practice

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,4 +1,8 @@
+//bcrypt-nodejs 사용
+//cors 사용!!
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,7 +12,6 @@ const database = {
 			id: '123',
 			name: 'John',
 			email: 'john@gmail.com',
-			password: '123',
 			entries: 0,
 			joined: new Date()
 		},
@@ -16,9 +19,15 @@ const database = {
 			id: '124',
 			name: 'Sally',
 			email: 'sally@gmail.com',
-			password: '124',
 			entries: 0,
 			joined: new Date()
+		}
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gmail.com'
 		}
 	]
 }
@@ -27,12 +36,19 @@ const database = {
 // bodyparser 모듈 express에 업데이트
 // app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send('this is working');
+	res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
+	// bcrypt.compare("bacon", hash, function(err, res) {
+
+	// });
+	// bcrypt.compare("veggies", hash, function(err, res) {
+
+	// });
 	if(req.body.email === database.users[0].email &&
 		req.body.password === database.user[0].password){
 		res.json('success');
@@ -41,6 +57,51 @@ app.post('/signin', (req, res) => {
 	}
 })
 
-app.listen(3000, () => {
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	// bcrypt.hash("bacon", null, null, function(err, hash) {
+    	
+	// });
+	database.users.push({
+		id: '',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => { //:id 파라미터 가져옴
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+			found = true;
+			return res.json(user);
+		}
+	})
+	if(!found){
+		res.status(400).json('not found');
+	}
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	})
+	if(!found){
+		res.status(400).json('not found');
+	}
+})
+
+app.listen(3001, () => {
 	console.log('app is running');
 })
