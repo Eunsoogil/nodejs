73932a05d8c305b96e4ea4a5fd13bce73cb3d736
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-01-25 13:31:24 +0900

    connection between db to backend

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -3,6 +3,37 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1234',
+    database : 'smart-brain'
+  }
+});
+
+/*
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+});
+*/
+
+//console.log(db.select('*').from('users'));
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
 
 const app = express();
 
@@ -41,7 +72,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send(database.users);
+	// res.send(database.users);
+	res.send('work')
 })
 
 app.post('/signin', (req, res) => {
@@ -64,45 +96,73 @@ app.post('/register', (req, res) => {
 	// bcrypt.hash("bacon", null, null, function(err, hash) {
     	
 	// });
-	database.users.push({
-		id: '',
-		name: name,
+	// database.users.push({
+	// 	id: '',
+	// 	name: name,
+	// 	email: email,
+	// 	password: password,
+	// 	entries: 0,
+	// 	joined: new Date()
+	// })
+
+	db('users')
+	.returning('*')
+	.insert({
 		email: email,
-		password: password,
-		entries: 0,
+		name: name,
 		joined: new Date()
+	}).then(user => {
+		res.json(user[0]);
+	}).catch(err => {
+		res.status(400).json('unable to register') //존재할경우 400날림
 	})
-	res.json(database.users[database.users.length - 1]);
+	// .returning('*') return all columns
 })
 
 app.put('/profile/:id', (req, res) => { //:id 파라미터 가져옴
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			user.entries++
-			return res.json(user);
+	// database.users.forEach(user => {
+	// 	if(user.id === id){
+	// 		found = true;
+	// 		user.entries++
+	// 		return res.json(user);
+	// 	}
+	// })
+	db.select('*').from('users').where({
+		id: id
+	}).then(user => {
+		if(user.length){
+			res.json(user[0])
+		} else {
+			res.status(400).json('not found')
 		}
-	})
-	if(!found){
-		res.status(400).json('not found');
-	}
+		
+	}).catch(err => res.status(400).json('error getting user'))
+	// if(!found){
+	// 	res.status(400).json('not found');
+	// }
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
+	// let found = false;
+	// database.users.forEach(user => {
+	// 	if(user.id === id){
+	// 		found = true;
+	// 		user.entries++;
+	// 		return res.json(user.entries);
+	// 	}
+	// })
+	// if(!found){
+	// 	res.status(400).json('not found');
+	// }
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
 	})
-	if(!found){
-		res.status(400).json('not found');
-	}
+	.catch(err => res.status(400).json('not found'))
 })
 
 app.listen(3001, () => {
